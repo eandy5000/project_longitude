b62789a1319538a192c309f8a9125d3ce1cab4dd
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2016-05-16 09:12:54 -0500

    inital commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,21 @@
+class ArticlesController < ApplicationController
+    def new
+        @article = Article.new
+    end
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice]="Article was saved"
+            redirect_to articles_path(@article)
+        else
+            render 'new'    
+        end
+    end
+    
+private
+    def article_params
+    #gets title and description
+    params.require(:article).permit(:title, :description)
+    end
+    
+end
---
+++ db/migrate/20160516130415_add_datetime.rb
@@ -0,0 +1,6 @@
+class AddDatetime < ActiveRecord::Migration
+  def change
+    add_column :articles, :updated_at, :datetime
+    add_column :articles, :created_at, :datetime
+  end
+end
