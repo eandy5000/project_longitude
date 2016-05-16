1fed92bd54490d6b18611cde645708eea2c7fa97
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2016-05-16 11:15:27 -0500

    basic crud working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,39 @@ class ArticlesController < ApplicationController
         end
     end
     
+    def show
+        @article = Article.find(params[:id])
+    end    
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        
+        if @article.update(article_params)
+            flash[:notice] = "Article Successfully Updated"
+            redirect_to article_path(@article)
+        else    
+            render 'edit'
+        end
+    end
+    
+    def index
+        # this is plural @articles_path
+        @articles = Article.all
+    end
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article was deleted"
+        redirect_to articles_path
+    end
+    
+    
+    
 private
     def article_params
     #gets title and description
