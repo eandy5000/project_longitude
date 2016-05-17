9fa7858fe50755d033c9a3f204abc919f9bc1902
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2016-05-17 07:49:50 -0500

    added users and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+    validates :username, 
+    presence: true, 
+    uniqueness: {case_sensitive: false},
+    length: {minimum: 3, maximum: 25}
+
+#tested in rubular regex isn't perfect but it works
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i    
+    validates :email,
+    presence: true,
+    uniqueness: {case_sensitive: false},
+    format: {with: VALID_EMAIL_REGEX }
 end
