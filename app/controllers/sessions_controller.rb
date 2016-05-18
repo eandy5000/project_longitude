0539c330c64c9bda0880e169280096982e1dc035
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2016-05-18 11:59:10 -0500

    added logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,21 @@ class SessionsController < ApplicationController
     end
     
     def create
-        render 'new'
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+        session[:user_id] = user.id
+        flash[:success] = "You are logged in"
+        redirect_to user_path(user)
+        else
+            flash.now[:danger] = "Incorrect login credentials"
+            render 'new'
+        end
+        
     end
     
     def destroy
+        session[:user_id] = nil
+        flash[:success] = "You are logged out"
+        redirect_to root_path
     end
 end
